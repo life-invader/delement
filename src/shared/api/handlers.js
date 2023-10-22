bd6be3ade0a90cde915770f07b7e97cb7525c763
Alexander <orientstar96@gmail.com>
2023-10-22 19:55:19 +0500

    4.3 Добавляет хендлеры для MSW - Хендлеры добавления и удаления товара зи корзины

--- src/shared/api/handlers.js
+++ src/shared/api/handlers.js
@@ -7,8 +7,18 @@ export const handlers = [
     return res(ctx.json(products));
   }),
 
-  rest.post("/cart", (req, res, ctx) => {
+  rest.post("/cart", (_req, res, ctx) => {
     // Place new order
+    return res(ctx.json({ isSuccess: true }));
+  }),
+
+  rest.post("/cart/add", (_req, res, ctx) => {
+    // Add product to cart
+    return res(ctx.json({ isSuccess: true }));
+  }),
+
+  rest.delete("/cart", (_req, res, ctx) => {
+    // Remove product to cart
     return res(ctx.json({ isSuccess: true }));
   }),
 ];
